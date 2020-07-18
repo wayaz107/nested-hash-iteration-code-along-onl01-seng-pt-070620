97b20208c0074a1afe98329c100589a94bba9b06
Warda Ayaz <wayaz107@gmail.com>
2020-07-18 22:58:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,5 +16,9 @@ require 'pry'
 
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
-  binding.pry
+  if person == "Freddy Mercury"
+    contact_details_hash.each do
+      binding.pry
+    end
+    end
 end
